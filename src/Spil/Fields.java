7dc749fceaa6671d0f973335659f4934cd727a87
ahsunalla.patan@gmail.com <ahsunalla.patan@gmail.com>
2022-10-25 12:20:21 +0200

    Ændret i fields, så den trækker med over i game

--- src/Spil/Fields.java
+++ src/Spil/Fields.java
@@ -4,7 +4,8 @@ public class Fields {
     public Fields(){}
     int[] placesPoint = {0, 250,  -100,  100,  -20,  180,  0,  -70, 60,  -80, -50,  650};
 
-    String[][] placesString = {{"Tower, Godt gået! Du har modtaget 250 point"}, {"Crater, Desværre! du har mistet 100 point"},
+    String[][] placesString = {{"Tower, Godt gået! Du har modtaget 250 point"},
+            {"Crater, Desværre! du har mistet 100 point"},
             {"palaceGates, Tillykke! du har modtaget 100 point"},
             {"coldDesert, Åh nej! du har mistet 20 point"},
             {"walledCity, Sådan! du har mogtaget 180"},
@@ -15,42 +16,7 @@ public class Fields {
             {"thePit, Du har mistet 50 point"},
             {"Goldmine, Tillykke du har modtaget 650 point"}};
 
-    public int getSquare(int sumDice){
-        return placesPoint[sumDice];
-
-    }
-
-    public String getTower(int sumDice){
-        return placesPoint[sumDice] + " " + placesString[sumDice-1][1];
-    }
-    public int getCrater(int sumDice) {
-        return placesPoint[1];
-    }
-    public int getPalaceGates() {
-        return placesPoint[2];
-    }
-    public int getColdDesert() {
-        return placesPoint[3];
-    }
-    public int getWalledCity() {
-        return placesPoint[4];
-    }
-    public int getMonastery() {
-        return placesPoint[5];
-    }
-    public int getBlackCave() {
-        return placesPoint[6];
-    }
-    public int getHutsInTheMountain() {
-        return placesPoint[7];
-    }
-    public int getTheWerewall() {
-        return placesPoint[8];
-    }
-    public int getThePit() {
-        return placesPoint[9];
-    }
-    public int getGoldmine() {
-        return placesPoint[10];
+    public String getSquare(int sumDice){
+        return placesPoint[sumDice] + " " + placesString[sumDice-1][0];
     }
 }
--- src/Spil/Game.java
+++ src/Spil/Game.java
@@ -5,10 +5,14 @@ public class Game {
 
     public void Play() {
         int round = 1;
+
         Player Player1 = new Player("Player1");
         Player Player2 = new Player("Player2");
+
         Fields places = new Fields();
+
         Dice dice = new Dice();
+
         System.out.println("Spillet er nu begyndt");
 
         while(Player1.account.getAccount() < 3000 || Player2.account.getAccount() < 3000) {
@@ -17,7 +21,7 @@ public class Game {
                     Player1.setPosition(Player1.getPosition() + dice.roll(2));
                 }
 
-                System.out.println(places.getTower(Player1.getPosition()));
+                System.out.println(places.getSquare(Player1.getPosition()));
                 System.out.println("Player1 du er landet på plads: " + Player1.getPosition());
                 if (Player1.account.getAccount() > 3000) {
                     Player1.account.setAccount(3000);
