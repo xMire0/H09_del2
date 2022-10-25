570fbc2d2bbbcb184ba478cd40e0228700352b31
Emir-D <emir@degirmenci.dk>
2022-10-25 15:37:33 +0200

    Ændret strings til Engelsk fra dansk
    Tilføjet sleep
    
    Rettet i strings således det ser visuelt bedre ud i konsollen.

--- src/Spil/Game.java
+++ src/Spil/Game.java
@@ -34,33 +34,39 @@ public class Game {
         while(Player1.account.getAccount() < 3000 || Player2.account.getAccount() < 3000) {
             if (round % 2 == 1) {
                 if (Player1.account.getAccount() < 3000) {
-
+                    System.out.println("Your account balance is: " + Player2.account.getAccount());
                     System.out.print(Player1.getName() + "'s turn to throw the dice.\nPress enter to throw dices\n");
+                    System.out.println("Your account balance is: " + Player2.account.getAccount());
                     sc.nextLine();
                     Player1.setPosition(Player1.getPosition() + dice.roll(2));
                 }
 
                 System.out.println("Player1 you've landed on the field: " + places.getSquare(Player1.getPosition()));
                 System.out.print("\n");
-                // der skal laves get account her System.out.println("Your new balance is: " + Player1.getAccount());
                 System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
                 System.out.println("\n");
 
                 if (Player1.account.getAccount() > 3000) {
                     Player1.account.setAccount(3000);
+
                 }
                 else if (Player2.account.getAccount() < 3000){
+                    System.out.println("Your account balance is: " + Player2.account.getAccount());
                     Player2.setPosition(Player2.getPosition() + dice.roll(2));
                     System.out.print(Player2.getName() + "'s turn to throw the dice.\nPress enter to throw dices\n");
                     sc.nextLine();
+                    Player2.setPosition(Player2.getPosition() + dice.roll(2));
+                    System.out.println("You've rolled: " + );
                 }
+
                 System.out.println("Player2 you've landed on the field: " + places.getSquare(Player2.getPosition()));
                 if (Player2.account.getAccount() > 3000) {
                     Player2.account.setAccount(3000);
                 }
-                break;
+
             }
             round++;
+            break;
         }
     }
 }
